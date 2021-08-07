ce1a001d9a21245bf9dd00c783f414558c54b445
Marat Suyargulov <ml65@rambler.ru>
2021-08-07 12:50:10 +0300

    Update web.php

--- config/web.php
+++ config/web.php
@@ -8,6 +8,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'defaultRoute' => 'cars/index',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
