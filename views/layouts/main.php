8ee55a6bb87fd9985d4e184a7420d84f47743f70
Marat Suyargulov <ml65@rambler.ru>
2021-08-07 12:51:14 +0300

    Update main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -38,8 +38,7 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'Cars', 'url' => ['/cars/index']],
+            ['label' => 'Catalog', 'url' => ['/cars/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
